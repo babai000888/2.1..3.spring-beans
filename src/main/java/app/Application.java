ac766fe912500f2dda9c7284d6262427fd8adc87
babai000888 <47792371+babai000888@users.noreply.github.com>
2021-08-12 23:58:11 +0300

    №1

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,6 +4,8 @@ import app.config.AppConfig;
 import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
 
 public class Application {
 
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,16 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("prototype")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-
     public Long getTime() {
         return nanoTime;
     }
